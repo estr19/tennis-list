d2cfba2e92b90c07a59ed3f30fba6ed4f3b2f905
Elena Strakhova <elenastrakhova@gmail.com>
2022-01-11 20:40:53 -0500

    strike-through changes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="app">
       <div className='container'>
-        <img src={court} width="400px" alt="groceries" />
+        <img src={court} width="300px" alt="groceries" />
       </div>
       <h1 className='container'>Are you ready for your next match?</h1>
-      <h4>Click once to cross the item off the list, double-click to remove the item.</h4>
+      <h4>Click once to cross the item off the list and move it to the bottom, double-click to remove the item.</h4>
       <Tennis />
     </div>
   );
