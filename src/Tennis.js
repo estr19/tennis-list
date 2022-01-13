f5a4b6ec3c3cf98edfd2ee6596fc1b0b3d16dd33
Elena Strakhova <elenastrakhova@gmail.com>
2022-01-13 11:58:12 -0500

    two lists plus dblclck

--- src/Tennis.js
+++ src/Tennis.js
@@ -4,9 +4,15 @@ import Swal from 'sweetalert2'
 import alert from 'sweetalert2-react-content'
 
 export class Tennis extends Component {
-  state = {
-    userInput: '',
-    groceryList: []
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+      tennisList: [],
+      completedList: [],
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDoneClick = this.handleDoneClick.bind(this);
   }
 
   onChangeEvent(e) {
@@ -23,58 +29,84 @@ export class Tennis extends Component {
       })
     }
     else {
-      let listArray = this.state.groceryList;
+      let listArray = this.state.tennisList;
       listArray.push(input);
-      this.setState({groceryList: listArray, userInput: ''});
+      this.setState({tennisList: listArray, userInput: ''});
+      console.log(listArray);
     }
   }
 
-  crossedOut(e) {
-    const li = e.target;
-    li.classList.toggle('crossed');
-    let listArray = this.state.groceryList;
-    listArray.splice(listArray.indexOf(e.target.innerText), 1);
-    this.setState({
-      groceryList: listArray
-    })
-  }
-
-  delOneItem(e) {
-    let ToDoArray = this.state.groceryList;
-    ToDoArray.splice(e.index, 1);
-    this.setState({ToDoList: ToDoArray});
-  }
-
   deleteAll() {
-    let listArray = this.state.groceryList;
+    let listArray = this.state.tennisList;
+    let complete = this.state.completedList;
     listArray = [];
-    this.setState({groceryList: listArray});
+    complete = [];
+    this.setState({tennisList: listArray, completedList: complete});
   }
 
   submitForm(e) {
     e.preventDefault();
   }
 
-  // handleClick(e) {
-  //   switch (e.detail) {
-  //     case 1:
-  //       const li = e.target;
-  //       li.classList.toggle('crossed');
-  //       console.log(li.indexOf());
-  //       // console.log("click");
-  //       break;
-  //     case 2:
-  //       const lii = e.target;
-  //       lii.classList.add('removed');
-  //       break;
-  //     // case 3:
-  //     //   const liii = e.target;
-  //     //   liii.classlist.add('framed');
-  //     //   // console.log("triple click");
-  //     //   break;
-  //     default:
-  //   }
-  // };
+  handleClick(e) {
+    const li = e.target;
+    let doneList = this.state.tennisList;
+    let complete = this.state.completedList;
+    switch (e.detail) {
+      case 1:
+        let pushedDown = li.innerText;
+        let index = doneList.indexOf(pushedDown);
+        let newIndex = doneList.length - 1;
+        console.log(index);
+        console.log(newIndex);
+        console.log(doneList[index]);
+        complete.push(pushedDown);
+        doneList.splice(doneList.indexOf(li.innerText), 1);
+        this.setState({
+          tennisList: doneList
+        })
+        console.log(doneList);
+        break;
+      case 2:
+        console.log(doneList);
+        doneList.splice(doneList.indexOf(li.innerText), 1);
+        this.setState({
+          completedList: complete
+        })
+        break;
+      default:
+    }
+  };
+
+  handleDoneClick(e) {
+    const li = e.target;
+    let doneList = this.state.tennisList;
+    let complete = this.state.completedList;
+    switch (e.detail) {
+      case 1:
+        let pushedUp = li.innerText;
+        let index = complete.indexOf(pushedUp);
+        let newIndex = complete.length - 1;
+        console.log(index);
+        console.log(newIndex);
+        console.log(complete[index]);
+        doneList.push(pushedUp);
+        complete.splice(complete.indexOf(li.innerText), 1);
+        this.setState({
+          completedList: complete
+        })
+        console.log(doneList);
+        break;
+      case 2:
+        console.log(complete);
+        complete.splice(complete.indexOf(li.innerText), 1);
+        this.setState({
+          tennisList: doneList
+        })
+        break;
+      default:
+    }
+  };
 
   render() {
     return(
@@ -90,11 +122,15 @@ export class Tennis extends Component {
             <button className='btn delete' type="button" onClick={() => {this.deleteAll()}}>Clear All</button>
             <button className='btn add' type="submit" onClick={() => {this.addItem(this.state.userInput)}}>Add</button>
           </div>
-          <div className='container'>
-            <ul id="list">
-              {this.state.groceryList.map((item, index) => (
-                <li key={index} onDoubleClick={(e) => this.delOneItem({index})} onClick={this.crossedOut}>&nbsp;{item}</li>
-                // <li onClick={this.handleClick} key={index}>&nbsp;{item}</li>
+          <div id='bullets' className='container'>
+            <ul id='list'>
+              {this.state.tennisList.map((item, index) => (
+                <li className='paragraph' onClick={this.handleClick} key={index}>{item}</li>
+              ))}
+            </ul>
+            <ul id='done'>
+              {this.state.completedList.map((item, index) => (
+                <li className='paragraph' onClick={this.handleDoneClick} key={index}>{item}</li>
               ))}
             </ul>
           </div>
